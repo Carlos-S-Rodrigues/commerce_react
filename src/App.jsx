56830ec9acd64db7b2fe5a03d2471bd5ade163dc
Carlos-S-Rodrigues <carlos.schuenck.rodrigues@gmail.com>
2022-10-24 08:53:10 -0300

    feat: commerce_react - project start

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import GlobalStyle from './globalStyle'
+import Home from './pages/Home'
+
+
+function App() {
+  
+
+  return (
+    <>
+      <GlobalStyle/>
+      <Home/>
+    </>
+  )
+}
+
+export default App
